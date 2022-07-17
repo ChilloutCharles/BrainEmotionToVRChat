75ab03a00727a3bd888939049b40761e19ff0440
chillout_charles <blargophobia@gmail.com>
2022-07-16 19:04:59 -0500

    fixed absolute path for model

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
     DataFilter.enable_data_logger()
 
     ### Uncomment this to see debug messages ###
-    # BoardShim.set_log_level(LogLevels.LEVEL_DEBUG.value)
+    BoardShim.set_log_level(LogLevels.LEVEL_DEBUG.value)
 
     ### Paramater Setting ###
     parser = argparse.ArgumentParser()
@@ -127,8 +127,7 @@ def main():
     ### ONNX Setup ###
     model_params = BrainFlowModelParams(BrainFlowMetrics.USER_DEFINED.value,
                                         BrainFlowClassifiers.ONNX_CLASSIFIER.value)
-    model_params.file = os.path.join(
-        os.getcwd(), 'ml-spotify', 'spotify_emotion.onnx')
+    model_params.file = os.path.join(os.getcwd(), 'spotify_emotion.onnx')
     model = MLModel(model_params)
     model.prepare()
 
